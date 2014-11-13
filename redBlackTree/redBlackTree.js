c0a8bddfed2010cc2af381c1c4cb8b3b081fe3f0
Christopher Treviño <christophertrev@gmail.com>
2014-11-12 22:19:42 -0800

    Add test sweet for redBlackTree.js

---
+++ redBlackTree/redBlackTree.js
@@ -0,0 +1,7 @@
+var RedBlackTree = function (){
+  this.left = null,
+  this.right = null,
+  this.color = black,
+  this.parent = null
+}
+
---
+++ redBlackTree/spec/redBlackTree.js
@@ -0,0 +1,20 @@
+/* global redBlackTree, describe, it, expect, should */
+
+describe('redBlackTree()', function () {
+  //'use strict';
+
+  it('exists', function () {
+    expect(RedBlackTree).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
